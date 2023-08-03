0d89b046ed04f4a325ac363c81c3b08a0bd4bb58
saliiimm <salimghalem40@gmmail.com>
2023-08-03 23:33:11 +0100

    organisation

---
+++ src/components/home/acceuil/Acceuil.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Acceuil.css";
+
+const Acceuil = () => {
+  return <div>Acceuil</div>;
+};
+
+export default Acceuil;
---
+++ src/components/home/learn_more/LearnMore.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./LearnMore.css";
+
+const LearnMore = () => {
+  return <div>LearnMore</div>;
+};
+
+export default LearnMore;
