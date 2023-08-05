cf9acf378cb065962e8c88835757a626519d85ba
saliiimm <salimghalem40@gmmail.com>
2023-08-05 18:38:08 +0100

    navbar + icon title

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,31 @@
 import "./Navbar.css";
-
+import { BsFillPersonFill } from "react-icons/bs";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import logoNav from "../../assets/logoNav.svg";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div className="partieGauche">
+        <Link>
+          <img src={logoNav} alt="logo Candles.Inc" />
+        </Link>
+      </div>
+      <div className="centre">
+        <h5>Discovery</h5>
+        <h5>About</h5>
+        <h5>Contact us</h5>
+      </div>
+      <div className="partieDroit">
+        <Link>
+          <BsFillPersonFill />
+        </Link>
+        <Link to="/panier">
+          <AiOutlineShoppingCart />
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
