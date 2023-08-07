1e57353434434779773ab423c42dd99306b5340e
Medjber Soundous <101836665+medjbersoundous@users.noreply.github.com>
2023-08-07 12:41:56 +0100

    Add files via upload

--- src/pages/panier/Panier.jsx
+++ src/pages/panier/Panier.jsx
@@ -1,15 +1,52 @@
 import Navbar from "../../components/navbar/Navbar";
 import Footer from "../../components/footer/Footer";
-import Modal from "../../components/modalPayment/Modal";
+import "./Panier.css"
+import { Link } from "react-router-dom";
+const Panier = ({ cartItems }) => {
+ 
 
-const Panier = () => {
   return (
-    <>
+    < div className="back" >
       <Navbar />
-      <Modal />
+      <div>
+        <h2  >Your card items</h2>
+         <Link className="a" to="/">Back to shopping</Link>
+      </div>
+      <div>
+        <div className="shop">
+          <h4>Product</h4>
+          <div className="shopp">
+            <h4>Price</h4>
+            <h4>Quantity</h4>
+            <h4>Total</h4>
+          </div>
+        </div>
+        <hr className="hr" />
+        <div>
+          {cartItems.map((item) => (
+            <div key={item.key}>
+              <p>{item.candleName}</p>
+              <p>${item.price}</p>
+        
+            </div>
+          ))}
+        </div>
+        <hr className="hr" />
+        <div className="rights">
+          <div className="pay" >
+            <div className="align">
+              <h4>Sub-total</h4>
+              <h4>$9.99</h4>
+            </div>
+            <p className="p">tax and shipping cost will calculated later</p>
+          </div>
+          <button className="button" >Check-out</button>
+        </div>
+      </div>
       <Footer />
-    </>
+    </div>
   );
 };
 
 export default Panier;
+
