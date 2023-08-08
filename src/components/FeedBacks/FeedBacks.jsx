8dcf0648eb1e2ceb987c7637013eee032679c473
saliiimm <salimghalem40@gmmail.com>
2023-08-08 22:28:23 +0100

    details

--- src/components/FeedBacks/FeedBacks.jsx
+++ src/components/FeedBacks/FeedBacks.jsx
@@ -1,68 +1,70 @@
-import React from 'react'
-import './FeedBacks.css'
+import React from "react";
+import "./FeedBacks.css";
 import image1 from "../../assets/avis2.svg";
 import image2 from "../../assets/avis1.svg";
 import image3 from "../../assets/avis3.svg";
-import {AiFillStar} from 'react-icons/ai'
+import { AiFillStar } from "react-icons/ai";
 const FeedBacks = () => {
   return (
-    <div className='green'>
+    <div className="green">
       <div>
-        <h2 className='titlef' >Testimonials</h2 >
-        <p className='titlep'> Some quates from our happy customers</p>
+        <h2 className="titlef">Testimonials</h2>
+        <p className="titlep"> Some quotes from our happy customers</p>
       </div>
-    <div className='greens'>
-      <div className='card-feed'>
-        <div>
-          <img src={image1} alt="" />
+      <div className="greens">
+        <div className="card-feed">
           <div>
-          <AiFillStar className='stars' />
-          <AiFillStar className='stars' />
-          <AiFillStar className='stars'/>
-          <AiFillStar className='stars' />
-          <AiFillStar className='star' />
-          </div>
-          <div>
-            <p className='feedb'>“I love it! No more air <br /> fresheners”</p>
-            <p  className='feedname'>Luisa</p>
+            <img src={image1} alt="" />
+            <div>
+              <AiFillStar className="stars" />
+              <AiFillStar className="stars" />
+              <AiFillStar className="stars" />
+              <AiFillStar className="stars" />
+              <AiFillStar className="star" />
+            </div>
+            <div>
+              <p className="feedb">
+                “I love it!just don't buuy Grey parfum! <br /> Horrible one”
+              </p>
+              <p className="feedname">Luisa</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className='card-feed'>
-        <div>
-          <img src={image2} alt="" />
+        <div className="card-feed">
           <div>
-          <AiFillStar  className='stars'/>
-          <AiFillStar className='stars' />
-          <AiFillStar className='stars'/>
-          <AiFillStar className='stars'/>
-          <AiFillStar className='stars'/>
-          </div>
-          <div>
-            <p className='feedb'>“Raccomended for  <br /> everyone”</p>
-            <p className='feedname'>Edoardo</p>
+            <img src={image2} alt="" />
+            <div>
+              <AiFillStar className="stars" />
+              <AiFillStar className="stars" />
+              <AiFillStar className="stars" />
+              <AiFillStar className="stars" />
+              <AiFillStar className="stars" />
+            </div>
+            <div>
+              <p className="feedb">“sl3a chaba l3ziz”</p>
+              <p className="feedname">Edoardo</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className='card-feed'>
-        <div>
-          <img src={image3} alt="" />
+        <div className="card-feed">
           <div>
-          <AiFillStar className='stars' />
-          <AiFillStar className='stars'/>
-          <AiFillStar className='stars'/>
-          <AiFillStar className='stars'/>
-          <AiFillStar className='star'/>
-          </div>
-          <div>
-            <p className='feedb'>“Looks very natural, the smell is awesome”</p>
-            <p  className='feedname'>Mart</p>
+            <img src={image3} alt="" />
+            <div>
+              <AiFillStar className="stars" />
+              <AiFillStar className="stars" />
+              <AiFillStar className="stars" />
+              <AiFillStar className="stars" />
+              <AiFillStar className="star" />
+            </div>
+            <div>
+              <p className="feedb">“Vraiment haja le top!Mabenna”</p>
+              <p className="feedname">Mart</p>
+            </div>
           </div>
         </div>
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default FeedBacks
+export default FeedBacks;
--- src/pages/panier/Panier.jsx
+++ src/pages/panier/Panier.jsx
@@ -11,7 +11,7 @@ const Panier = () => {
   const [candles, setCandles] = useState([]);
   const [isModalVisible, setIsModalVisible] = useState(false);
   let subtotal = 0;
- 
+
   useEffect(() => {
     axios
       .get("panier.json")
@@ -55,75 +55,90 @@ const Panier = () => {
     <div className="back">
       <Navbar />
       {isModalVisible && <Modal candels={candles} onClose={closeModal} />}
-      <div className="topp">
-        <h2>Your cart items</h2>
-        <Link className="a" to="/">
-          Back to shopping
-        </Link>
-      </div>
-      <div>
-        <div className="shop">
-          <h4>Product</h4>
-          <div className="shopp">
-            <h4>Price</h4>
-            <h4>Quantity</h4>
-            <h4>Total</h4>
-          </div>
+
+      {candles.length === 0 ? ( // If no items in the cart
+        <div className="no-items">
+          <h2>Your cart is empty.</h2>
+          <Link to="/">
+            <h3>Go back to shopping</h3>
+          </Link>
         </div>
-        <hr className="hr hrs" />
-        <div className="itemContent">
-          {candles.map((item) => {
-            const total = (item.price * item.quantity).toFixed(2);
-
-            subtotal += parseFloat(total);
-
-            return (
-              <div className="shop">
-                <div className="flexx">
-                  <img src={item.image} className="small" alt={item.name} />
-                  <div className="pro">
-                    <p className="isem">{item.name}</p>
-
-                    <button
-                      className="remove"
-                      onClick={() => remove(item.id)}
-                    />
-
-                    <button
-                      className="remove"
-                      onClick={() => handleDelete(item.id)}>
-                      Remove
-                    </button>
-                  </div>
-                </div>
-                <div className="shopp">
-                  <p>${item.price}</p>
-                  <p>{item.quantity}</p>
-                  <p>${total}</p>
-                </div>
+      ) : (
+        <>
+          <div className="topp">
+            <h2>Your cart items</h2>
+            <Link className="a" to="/">
+              Back to shopping
+            </Link>
+          </div>
+          <div>
+            <div className="shop">
+              <h4>Product</h4>
+              <div className="shopp">
+                <h4>Price</h4>
+                <h4>Quantity</h4>
+                <h4>Total</h4>
               </div>
-            );
-          })}
-        </div>
-        <hr className="hr" />
-        <div className="rights">
-          <div className="pay">
-            <div className="align">
-              <h4>Sub-total</h4>
-              <h4>${subtotal.toFixed(2)}</h4>
             </div>
-            <p className="p">Tax and shipping cost will be calculated later</p>
+            <hr className="hr hrs" />
+            <div className="itemContent">
+              {candles.map((item) => {
+                const total = (item.price * item.quantity).toFixed(2);
+
+                subtotal += parseFloat(total);
+
+                return (
+                  <div className="shop">
+                    <div className="flexx">
+                      <img src={item.image} className="small" alt={item.name} />
+                      <div className="pro">
+                        <p className="isem">{item.name}</p>
+
+                        <button
+                          className="remove"
+                          onClick={() => remove(item.id)}
+                        />
+
+                        <button
+                          className="remove"
+                          onClick={() => handleDelete(item.id)}>
+                          Remove
+                        </button>
+                      </div>
+                    </div>
+                    <div className="shopp">
+                      <p>${item.price}</p>
+                      <p>{item.quantity}</p>
+                      <p>${total}</p>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+            <hr className="hr" />
+            <div className="rights">
+              <div className="pay">
+                <div className="align">
+                  <h4>Sub-total</h4>
+                  <h4>${subtotal.toFixed(2)}</h4>
+                </div>
+                <p className="p">
+                  Tax and shipping cost will be calculated later
+                </p>
+              </div>
+              <button
+                className="button"
+                onClick={() => {
+                  setIsModalVisible(true);
+                  openModal();
+                }}>
+                Check-out
+              </button>
+            </div>
           </div>
-          <button
-            className="button"
-            onClick={() => {
-              setIsModalVisible(true);
-              openModal();
-            }}>
-            Check-out
-          </button>
-        </div>
-      </div>
+        </>
+      )}
+
       <Footer />
     </div>
   );
